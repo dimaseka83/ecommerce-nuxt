9b49d13287806ea4201c74bb7c65eb9614dae9ca
dhimaseka <dimaseka83@gmail.com>
2022-03-30 14:26:32 +0700

    ++

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Nav/>
+    <HomeCarousel :sale_items="sale_items"/>
+    <br/><br/>
+    <v-container >
+      <h1 class="text-md-h4 text-h6">Check these out</h1>
+      <br/>
+      <ProductSlider :products="products"/>
+    <br/><br/><br/>
+    <Newsletter/>
+    </v-container>
+    <Footer/>
+    <ScrollTop/>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      products: null,
+      sale_items: null
+    }
+  },
+  async created() {
+    this.sale_items = await this.$content('products').where({onSale: true}).fetch();
+    this.products = await this.$content('products').fetch();
+  },
+}
+</script>
---
+++ pages/products/index.vue
@@ -0,0 +1,78 @@
+<template>
+    <div>
+        <Nav/>
+        <v-container>
+            <v-row dense>
+                <v-col md="3">
+                    <div>
+                        <v-text-field prepend-inner-icon="mdi-magnify" outlined clearable placeholder="Search" v-model="search"></v-text-field>
+                        <v-list subheader color="transparent" v-if="$vuetify.breakpoint.mdAndUp">
+                            <v-subheader>Categories</v-subheader>
+                            <v-list-item v-for="(c,i) in categories" :key="`category${i}`" link>
+                                <v-list-item-avatar>
+                                    <v-img :src="c.image"></v-img>
+                                </v-list-item-avatar>
+                                <v-list-item-content>
+                                    <v-list-item-title>
+                                        {{ c.name }}
+                                    </v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-list>
+                    </div>
+                </v-col>
+                <v-col md="9">
+                    <v-row>
+                        <template v-for="(p, i) in filteredProducts">
+                            <v-col cols="12" md="6" :key="`product${p.id}-${i}`">
+                                <v-card nuxt :to="`/products/${p.id}`" color="surface" class="el ma-2 mb-5 mr-5">
+                                    <v-img :src="p.image" height="300">
+                                        <template #placeholder>
+                                            <v-row class="fill-height" justify="center" align="center">
+                                                <v-progress-circular width="2" size="100" color="primary" indeterminate></v-progress-circular>
+                                            </v-row>
+                                        </template>
+                                    </v-img>
+                                    <v-card-title class="text-md-body-1 font-weight-bold">{{p.name}}</v-card-title>
+                                    <v-card-subtitle class="primary--text pb-3">{{ $formatMoney(p.price)}}</v-card-subtitle>
+                                        <v-chip x-small label outlined class="mr-1" v-for="(t,f) in p.tags" :key="`prod${p.id}-${f}`">{{t}}</v-chip>
+                                </v-card>
+                            </v-col>
+                        </template>
+                    </v-row>
+                </v-col>
+            </v-row>
+        </v-container>
+        <Footer/>
+        <ScrollTop />
+    </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      products: null,
+      categories: null,
+      search: null
+    }
+  },
+  async created() {
+    this.categories = await this.$content('category').fetch();
+    this.products = await this.$content('products').fetch();
+  },
+  computed: {
+      filteredProducts(){
+          if (!this.search || !this.products) return this.products || [];
+          return this.products.filter((p) => {
+              const s = this.search.toLowerCase();
+              const n = p.name.toLowerCase();
+              const price = p.price.toString();
+              const sprice = p.salePrice?.toString() || "";
+              const r = p.ratings.toString();
+
+              return n.includes(s) || price.includes(s) || sprice.includes(s) || r.includes(s);
+          })
+      }
+  }
+}
+</script>
